42273f7f396affafedd519b9e725a71a107c5c29
amaanthink <amaan.sheikh@thinkitive.com>
2024-01-30 19:03:16 +0530

    feat : add async-await

--- src/components/FetchAPI.jsx
+++ src/components/FetchAPI.jsx
@@ -1,32 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const FetchAPI = () => {
   const [photos, setPhotos] = useState([]);
 
-  useEffect(() => {
-    fetch('https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=20')
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setPhotos(data.photos); 
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  }, []);
+  async function fetchPhotos() {
+    try {
+      const res = await fetch('https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=20');
+     
+      const data = await res.json();
+     
+      setPhotos(data.photos);
+    }
+     catch (error) {
+      console.error('Error:', error);
+    }
+  }
+//Here call the api
+  fetchPhotos();
 
   return (
     <>
       <h1>Using Fetch API</h1>
-      {photos.map((photo)=>{
-        return(
+      {photos.map((photo) => (
         <div key={photo.id}>
-            <img src={photo.url} alt={photo.title} />
-            <p>{photo.description}</p>
-            <h2>{photo.title}</h2>
-
+          <img src={photo.url} alt={photo.title} width={100} />
+          <p>{photo.description}</p>
+          <h2>{photo.title}</h2>
         </div>
-     ) })}
+      ))}
     </>
   );
 };
