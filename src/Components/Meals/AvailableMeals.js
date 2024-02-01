01df84afbc40baa1d73d554ed3e6c6a8e3fbc4ad
amaanthink <amaan.sheikh@thinkitive.com>
2024-02-01 15:14:32 +0530

    feat : add http error

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -5,11 +5,15 @@ const AvailableMeals = () => {
 
   const  [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(()=>{
     document.title="Available Meals"
     const fetchMeals = async () =>{
       const res = await fetch('https://react-http-873cd-default-rtdb.firebaseio.com/meals.json');
+      if(!res.ok) {
+        throw new Error("Somrthing went wrong!");
+      }
       const data = await res.json();
 
       const loadedMeals = [];
@@ -22,7 +26,10 @@ const AvailableMeals = () => {
         setIsLoading(false);
       }
     }
-    fetchMeals();
+    fetchMeals().catch((error=>{
+      setIsLoading(false);
+      setHttpError(error.message);
+    }))
   },[]);
 
   if(isLoading){
@@ -30,12 +37,18 @@ const AvailableMeals = () => {
       <p>Loading...</p>
     </section>)
   }
+  if(httpError){
+    return(
+      <section>
+        <p style={{color:"red"}} >{httpError}</p>
+      </section>
+    )
+  }
     const dummyMeals =  meals.map(meal=>{
         return (
-            <li key={meal.id}>
+            <li style={{listStyleType:"none"}} key={meal.id}>
                 <h2>{meal.name}</h2>
                 <h3>{meal.description}</h3>
-
                 <h4>${meal.price}</h4>        
             </li>
             
