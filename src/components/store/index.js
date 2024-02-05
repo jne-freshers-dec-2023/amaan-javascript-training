8d4456f487f3fb7361079e6b64c965fabb2a3a25
amaanthink <amaan.sheikh@thinkitive.com>
2024-02-05 17:18:01 +0530

    feat : Connect Redux Toolkit

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,23 +1,60 @@
-import { legacy_createStore as createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "INC") {
-    return {
-      counter: state.counter + 1,
-    };
-  }
-  if (action.type === "DEC") {
-    return {
-      counter: state.counter - 1,
-    };
-  }
-  if(action.type === "INCFIVE"){
-    return{
-        counter: state.counter + action.amount,
+const initialState = {counter:0, showCounter:true}
+
+const counterSlice = createSlice({
+    name:  'app',
+    initialState,
+    reducers:{
+        increament(state){
+            state.counter++;
+        },
+        decreament(state){
+            state.counter--;
+        },
+        increase(state,action){
+            state.counter += action.payload;
+        },
+        toggle(state){
+            state.showCounter=!state.showCounter;
+        }
     }
-  }
+});
 
-  return state;
-};
-const store = createStore(counterReducer);
+
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === "INC") {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter:state.showCounter
+//     };
+//   }
+//   if (action.type === "DEC") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter:state.showCounter
+//     };
+//   }
+//   if(action.type === "INCFIVE"){
+//     return{
+//         counter: state.counter + action.payload,
+//         showCounter:state.showCounter
+//     }
+//   }
+//   if(action.type === "TOG"){
+//     return{
+//         counter:!state.counter,
+//         showCounter:state.showCounter
+//     }
+//   }
+
+//   return state;
+// };
+const store = configureStore({
+    reducer:{
+        counter:counterSlice.reducer,
+    }
+});
+export const counterActions = counterSlice.actions;
+
 export default store;
