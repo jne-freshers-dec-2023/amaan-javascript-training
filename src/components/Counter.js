81ca1b7d0999490798cc62ee412da7f0b90585a2
amaanthink <amaan.sheikh@thinkitive.com>
2024-02-05 13:00:13 +0530

    feat : Dispatch action & useSelector

---
+++ src/components/Counter.js
@@ -0,0 +1,31 @@
+import { useDispatch, useSelector } from "react-redux";
+import classes from './Counter.module.css';
+
+const Counter = () => {
+const dispatch = useDispatch();
+const counter = useSelector(state=> state.counter)
+
+
+  const incrementHandler=()=>{
+    dispatch({type:'INC'})
+  }
+  const decrementHandler=()=>{
+    dispatch({type:'DEC'});
+  }
+
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+}; 
+
+export default Counter;
---
+++ src/components/store/index.js
@@ -0,0 +1,19 @@
+import { legacy_createStore as createStore} from 'redux';
+
+const  counterReducer = (state={counter:0},action)=>{
+        if(action.type === 'INC'){
+            return{
+                counter:state.counter+1,
+            };
+        }
+       if(action.type==='DEC'){
+            return{
+                counter: state.counter-1,
+            }
+}
+return state;
+}
+const store = createStore(counterReducer);
+export default store; 
+
+  
